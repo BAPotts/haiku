23f79acfe58b0301dc034ba296e214c8920ff1a0
Beverly Potts <pottsbeverly@gmail.com>
2020-06-24 16:29:10 -0400

    Fail new test 'should create a poem object with three lines

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -7,4 +7,12 @@ describe('Poem', () => {
     const poem = new Poem("This is a poem");
     expect(poem.line1).toEqual("This is a poem");
   })
+  test('should create a poem object with three lines', () =>
+  {
+    const poem = new Poem("This is line 1", "This is line 2", "This is line 3");
+    expect(poem.line1).toEqual("This is line 1");
+    expect (poem.line2).toEqual("this is line 2");
+    expect (poem.line3).toEqual("this is line 3");
+  })
+
 })
