6913f680f61d50e3c8c83fa1518fbfcb51c058df
Beverly Potts <pottsbeverly@gmail.com>
2020-06-24 18:57:15 -0400

    Fail test of eSubtractor function. Unclear where problem

--- src/haiku.js
+++ src/haiku.js
@@ -9,5 +9,19 @@ export class Poem {
     this.line1 = this.line1.split(' ');
     this.line2 = this.line2.split(' ');
     this.line3 = this.line3.split(' ');
+  };
+
+  eSubtractor(){
+    const values = Object.values(this);
+  
+  
+    for (const value of values) {
+      for(let i=0;i<value.length;i++){
+        if(value[i].charAt[value[i].length-1] ===("e")){
+          console.log("found it");
+            value[i].splice((value[i].length-1), 1);
+        };
+      };
     };
-}
+  };
+};
