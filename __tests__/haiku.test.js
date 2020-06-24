1be4b62c2fa4d9bd232b017551f8a338bc24e35a
Beverly Potts <pottsbeverly@gmail.com>
2020-06-24 16:32:18 -0400

    Add line2 and line3 parameters to Poem constructor and pass second Poem test

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -11,8 +11,8 @@ describe('Poem', () => {
   {
     const poem = new Poem("This is line 1", "This is line 2", "This is line 3");
     expect(poem.line1).toEqual("This is line 1");
-    expect (poem.line2).toEqual("this is line 2");
-    expect (poem.line3).toEqual("this is line 3");
+    expect (poem.line2).toEqual("This is line 2");
+    expect (poem.line3).toEqual("This is line 3");
   })
 
 })
